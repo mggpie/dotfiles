8370453dec5d5ac0bc3627ce69366eec0e07e709
w23b07d28 <w23b07d28@users.noreply.github.com>
2025-12-23 21:19:59 +0100

    add upstream default configs
    - Sway: from swaywm/sway master
    - Waybar: from Alexays/Waybar master
    - Foot: from dnkl/foot master
    - Mako: from emersion/mako master
    - Fish: minimal (Tide installed by Ansible)
    
    Playbook now deploys from files/ instead of copying system configs

---
+++ files/fish/config.fish
@@ -0,0 +1,5 @@
+# Fish shell - minimal default config
+# Official example: https://github.com/fish-shell/fish-shell
+
+# No config needed - Fish works out of the box
+# Tide prompt will be installed by Ansible
