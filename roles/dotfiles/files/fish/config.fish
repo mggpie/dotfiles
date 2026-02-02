6c68c1a670d52afaabaa2434b1c1cd32e3912280
mggpie <mggpie@users.noreply.github.com>
2026-02-02 22:10:48 +0100

    screenshot function, pipewire autostart, micro clipboard
    - Add screenshot fish function in config.fish (save to Pictures/Screenshots with timestamp, copy to clipboard)
    - Add Print Screen binding in sway config to call screenshot function
    - Fix sway autostart: add wireplumber and pipewire-pulse exec
    - Change pavucontrol-qt to pavucontrol in pipewire.yml (GTK works with pipewire-pulse)
    - Add clipboard: external to micro settings.json for wl-clipboard integration
    - Fix whitespace in ssh config and pipewire.yml

--- roles/dotfiles/files/fish/config.fish
+++ roles/dotfiles/files/fish/config.fish
@@ -162,6 +162,18 @@ function usboff
     end
 end
 
+# Screenshot function - select area, save to Pictures/Screenshots, copy to clipboard
+function screenshot
+    # Create Screenshots directory if it doesn't exist
+    mkdir -p $HOME/Pictures/Screenshots
+
+    # Generate filename with timestamp
+    set filename $HOME/Pictures/Screenshots/(date +%Y%m%d-%H%M%S).png
+
+    # Take screenshot of selected area, save to file and copy to clipboard
+    grim -g (slurp) - | tee $filename | wl-copy
+end
+
 # Start sway on TTY1
 if test (tty) = /dev/tty1
     exec sway
