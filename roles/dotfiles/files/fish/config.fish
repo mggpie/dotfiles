22364a724355fef3d8a77ef6c9773d8b068bab05
mggpie <mggpie@users.noreply.github.com>
2026-02-03 23:03:45 +0100

    move notepad to PARA inbox

--- roles/dotfiles/files/fish/config.fish
+++ roles/dotfiles/files/fish/config.fish
@@ -23,7 +23,7 @@ alias vim "nvim"
 alias fontsearch "fc-list | grep -i"
 alias gpg-check "gpg --keyserver-options auto-key-retrieve --verify"
 alias gpg-retrieve "gpg --keyserver-options auto-key-retrieve --receive-keys"
-alias notepad "micro ~/me.md"
+alias notepad "micro ~/Documents/0-Inbox/notepad.md"
 
 alias grep "grep --color=auto"
 alias egrep "egrep --color=auto"
