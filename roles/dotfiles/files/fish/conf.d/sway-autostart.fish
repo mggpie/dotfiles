55b514b39192bc54864978745d24f8f758486a88
mggpie <mggpie@users.noreply.github.com>
2026-02-03 23:02:51 +0100

    fish: enable automatic sway start on TTY1 login

--- roles/dotfiles/files/fish/conf.d/sway-autostart.fish
+++ roles/dotfiles/files/fish/conf.d/sway-autostart.fish
@@ -1,5 +1,4 @@
 # Start sway on TTY1
-# Uncomment to enable automatic sway start on login to TTY1
-#if test (tty) = /dev/tty1
-#    exec sway
-#end
+if test (tty) = /dev/tty1
+    exec sway
+end
--- roles/dotfiles/files/fish/functions/move-downloads.fish
+++ roles/dotfiles/files/fish/functions/move-downloads.fish
@@ -6,7 +6,7 @@ function move-downloads --description "Move files from Downloads to Documents/0-
     if test -d $downloads; and test (count $downloads/*) -gt 0 2>/dev/null
         # Find files not modified in the last 30 minutes and move them
         find $downloads -maxdepth 1 -type f -mmin +30 -exec mv {} $inbox/ \; 2>/dev/null
-        
+
         # Also move directories (folders) not modified in the last 30 minutes
         find $downloads -maxdepth 1 -mindepth 1 -type d -mmin +30 -exec mv {} $inbox/ \; 2>/dev/null
     end
