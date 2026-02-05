0ecbfae51ff99d94929bbb1c345ff8b44b0e827f
mggpie <mggpie@users.noreply.github.com>
2026-02-05 23:26:30 +0100

    update fish config

--- roles/dotfiles/files/fish/conf.d/sway-autostart.fish
+++ roles/dotfiles/files/fish/conf.d/sway-autostart.fish
@@ -1,4 +1,4 @@
-# Start sway on TTY1
-if test (tty) = /dev/tty1
-    exec sway
+# Start sway on TTY1 (only if not already running)
+if test (tty) = /dev/tty1; and test -z "$DISPLAY"; and test -z "$WAYLAND_DISPLAY"
+    sway
 end
