dec2bc04dd1cffc2878fd3fe744f36259f8beb56
mggpie <mggpie@users.noreply.github.com>
2026-02-06 19:48:55 +0100

    startup adds telegram + messenger on ws9

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -2,7 +2,7 @@ function startup
     pkill -x code 2>/dev/null
     pkill -x wezterm-gui 2>/dev/null
     pkill -x firefox 2>/dev/null
-    sleep 0.5
+    pkill -x telegram-deskto 2>/dev/null
 
     # Workspace 1: VSCode (75%) + notepad terminal (25%)
     swaymsg 'workspace number 1'
@@ -12,11 +12,16 @@ function startup
     sleep 1
     swaymsg '[app_id="org.wezfurlong.wezterm"] resize set width 25 ppt'
 
-    # Workspace 2: Firefox
-    swaymsg 'workspace number 2'
-    swaymsg 'exec firefox'
-    sleep 1
+    # Workspace 2: Firefox (restores previous session)
+    swaymsg 'workspace number 2; exec firefox'
+
+    # Workspace 9: Messenger (left) + Telegram (right)
+    swaymsg 'workspace number 9'
+    swaymsg 'exec firefox --new-window https://www.messenger.com'
+    sleep 2
+    swaymsg 'exec telegram-desktop'
 
     # Return to workspace 1
+    sleep 1
     swaymsg 'workspace number 1'
 end
