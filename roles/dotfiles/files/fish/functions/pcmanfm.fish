ae3a8da8187c6b81d05f3f7caa466acdf68579be
mggpie <mggpie@users.noreply.github.com>
2026-02-06 19:44:48 +0100

    remove unused pcmanfm.fish, fix XF86Documents

--- roles/dotfiles/files/fish/functions/pcmanfm.fish
+++
@@ -1,7 +0,0 @@
-function pcmanfm --description "PCManFM file manager - defaults to ~/Documents"
-    if test (count $argv) -eq 0
-        command pcmanfm ~/Documents
-    else
-        command pcmanfm $argv
-    end
-end
