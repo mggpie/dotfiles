83bda9d0daf5db23d3cbc499c98aecc619ce3239
mggpie <mggpie@users.noreply.github.com>
2026-02-06 19:52:15 +0100

    fix telegram binary name in startup

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -2,7 +2,7 @@ function startup
     pkill -x code 2>/dev/null
     pkill -x wezterm-gui 2>/dev/null
     pkill -x firefox 2>/dev/null
-    pkill -x telegram-deskto 2>/dev/null
+    pkill -x Telegram 2>/dev/null
 
     # Workspace 1: VSCode (75%) + notepad terminal (25%)
     swaymsg 'workspace number 1'
@@ -14,12 +14,13 @@ function startup
 
     # Workspace 2: Firefox (restores previous session)
     swaymsg 'workspace number 2; exec firefox'
+    sleep 3
 
     # Workspace 9: Messenger (left) + Telegram (right)
     swaymsg 'workspace number 9'
     swaymsg 'exec firefox --new-window https://www.messenger.com'
     sleep 2
-    swaymsg 'exec telegram-desktop'
+    swaymsg 'exec Telegram'
 
     # Return to workspace 1
     sleep 1
