dcc9a1490c94157b3d1f816de3dca30d1e20b6f9
mggpie <mggpie@users.noreply.github.com>
2026-02-08 17:23:34 +0100

    fix: radio waits for network on boot, scratchpad notepad 80% size
    - radio: poll youtube.com instead of fixed sleep 3
    - notepad: resize 80ppt after spawn in startup.fish
    - for_window: keep simple (float + scratchpad), resize in startup

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -5,7 +5,9 @@ function startup
 
     # Scratchpad notepad (floating, hidden until Super+`)
     swaymsg 'exec wezterm start --class notepad -- micro ~/Desktop/0-Inbox/notepad.md'
-    sleep 1
+    sleep 2
+    swaymsg '[app_id="notepad"] resize set width 80ppt height 80ppt, move position center'
+    sleep 0.5
 
     # Workspace 1: Firefox (33% left) + VSCode (66% right)
     swaymsg 'workspace number 1'
