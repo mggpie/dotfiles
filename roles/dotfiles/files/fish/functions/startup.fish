f6283748059a4ef9b48232f7c1ca9f18f1eb0928
mggpie <mggpie@users.noreply.github.com>
2026-02-08 17:26:18 +0100

    tweak: scratchpad notepad 40% width

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -6,7 +6,7 @@ function startup
     # Scratchpad notepad (floating, hidden until Super+`)
     swaymsg 'exec wezterm start --class notepad -- micro ~/Desktop/0-Inbox/notepad.md'
     sleep 2
-    swaymsg '[app_id="notepad"] resize set width 80ppt height 80ppt, move position center'
+    swaymsg '[app_id="notepad"] resize set width 40ppt height 80ppt, move position center'
     sleep 0.5
 
     # Workspace 1: Firefox (33% left) + VSCode (66% right)
