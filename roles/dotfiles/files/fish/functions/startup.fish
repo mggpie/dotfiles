afbd95d3a5926f583fce8b35efe9abc97734c716
mggpie <mggpie@users.noreply.github.com>
2026-02-08 20:57:52 +0100

    startup: reorder - wezterm on ws2 first, scratchpad last

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -3,11 +3,10 @@ function startup
     pkill -x wezterm-gui 2>/dev/null
     pkill -x firefox 2>/dev/null
 
-    # Scratchpad notepad (floating, hidden until Super+`)
-    swaymsg 'exec wezterm start --class notepad -- micro ~/Desktop/0-Inbox/notepad.md'
-    sleep 2
-    swaymsg '[app_id="notepad"] resize set width 40ppt height 80ppt, move position center'
-    sleep 0.5
+    # Workspace 2: WezTerm (first, so it loads in the background)
+    swaymsg 'workspace number 2'
+    swaymsg 'exec wezterm'
+    sleep 1
 
     # Workspace 1: Firefox (33% left) + VSCode (66% right)
     swaymsg 'workspace number 1'
@@ -20,9 +19,8 @@ function startup
     sleep 2
     swaymsg '[app_id="Firefox"] resize set width 33 ppt'
 
-    # Workspace 2: WezTerm
-    swaymsg 'workspace number 2'
-    swaymsg 'exec wezterm'
-    sleep 1
-    swaymsg 'workspace number 1'
+    # Scratchpad notepad (floating, hidden until Super+`)
+    swaymsg 'exec wezterm start --class notepad -- micro ~/Desktop/0-Inbox/notepad.md'
+    sleep 2
+    swaymsg '[app_id="notepad"] resize set width 40ppt height 80ppt, move position center'
 end
