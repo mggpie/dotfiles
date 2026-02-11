b31eccd0500eb7f979cb0f7d6e89a4a6539adac3
mggpie <mggpie@users.noreply.github.com>
2026-02-11 16:02:20 +0100

    add ydotool to sway deps, wait for VSCode instead of Firefox in startup

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -6,21 +6,23 @@ function startup
     # Workspace 2: WezTerm (first, so it loads in the background)
     swaymsg 'workspace number 2'
     swaymsg 'exec wezterm'
-    sleep 1
 
     # Workspace 1: Firefox (33% left) + VSCode (66% right)
     swaymsg 'workspace number 1'
     swaymsg 'exec firefox'
-    while not swaymsg -t get_tree | grep -q '"app_id": "Firefox"'
+    swaymsg 'exec code'
+    while not swaymsg -t get_tree | grep -q '"name": "Visual Studio Code"'
         sleep 0.5
     end
-    sleep 1
-    swaymsg 'exec code'
-    sleep 2
     swaymsg '[app_id="Firefox"] resize set width 33 ppt'
 
     # Scratchpad notepad (floating, hidden until Super+`)
     swaymsg 'exec wezterm start --class notepad -- micro ~/Desktop/0-Inbox/notepad.md'
-    sleep 2
+    sleep 1
     swaymsg '[app_id="notepad"] resize set width 40ppt height 80ppt, move position center'
+    
+    # Fix scratchpad centering bug - simulate mouse movement
+    ydotool mousemove -a 960 540
+    sleep 0.1
+    ydotool mousemove -a 961 540
 end
