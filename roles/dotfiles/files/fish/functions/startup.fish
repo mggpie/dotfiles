a24a171d7eb284676e8a0fa34438e76186a34cf7
mggpie <mggpie@users.noreply.github.com>
2026-02-13 20:29:58 +0100

    add delays to startup function, widen scratchpad notepad

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -6,14 +6,16 @@ function startup
     # Workspace 1: Firefox (40% left) + VSCode (40% middle) + WezTerm (20% right)
     swaymsg 'workspace number 1'
     swaymsg 'exec firefox'
+    sleep 1
     swaymsg 'exec code'
-    swaymsg 'exec wezterm'
+    sleep 1
+    swaymsg 'exec wezterm start -- fish -c notepad'
     while not swaymsg -t get_tree | grep -q '"name": "Visual Studio Code"'
-        sleep 0.5
+        sleep 0.1
     end
     swaymsg '[app_id="Firefox"] resize set width 40 ppt'
     while not swaymsg -t get_tree | grep -q '"app_id": "org.wezfurlong.wezterm"'
-        sleep 0.5
+        sleep 0.1
     end
     swaymsg '[app_id="org.wezfurlong.wezterm"] resize set width 20 ppt'
 
