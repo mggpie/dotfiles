a1d10bc6146aaa4664e5b1733395813f9775f6f6
mggpie <mggpie@users.noreply.github.com>
2026-02-13 20:38:31 +0100

    wait for each window in startup before launching next

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -3,24 +3,26 @@ function startup
     pkill -x wezterm-gui 2>/dev/null
     pkill -x firefox 2>/dev/null
 
-    # Workspace 1: Firefox (40% left) + VSCode (40% middle) + WezTerm (20% right)
+    # Workspace 1: Firefox (left) + VSCode (middle) + WezTerm (right)
+    # Wait for each window before launching the next to guarantee tiling order
     swaymsg 'workspace number 1'
+
     swaymsg 'exec firefox'
     while not swaymsg -t get_tree | grep -q '"app_id": "firefox"'
         sleep 0.1
     end
+
     swaymsg 'exec code'
     while not swaymsg -t get_tree | grep -q '"name": "Visual Studio Code"'
         sleep 0.1
     end
+
     swaymsg 'exec wezterm start -- fish -c notepad'
     while not swaymsg -t get_tree | grep -q '"app_id": "org.wezfurlong.wezterm"'
         sleep 0.1
     end
-    swaymsg '[app_id="Firefox"] resize set width 40 ppt'
-    while not swaymsg -t get_tree | grep -q '"app_id": "org.wezfurlong.wezterm"'
-        sleep 0.1
-    end
+
+    swaymsg '[app_id="firefox"] resize set width 40 ppt'
     swaymsg '[app_id="org.wezfurlong.wezterm"] resize set width 20 ppt'
 
     # Scratchpad notepad (floating, hidden until Super+`)
