4780b5c3225799120103cc180a4cb4b969173e8e
mggpie <mggpie@users.noreply.github.com>
2026-02-13 17:04:07 +0100

    set window layout to 40/40/20

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -3,7 +3,7 @@ function startup
     pkill -x wezterm-gui 2>/dev/null
     pkill -x firefox 2>/dev/null
 
-    # Workspace 1: Firefox (33% left) + VSCode (34% middle) + WezTerm (33% right)
+    # Workspace 1: Firefox (40% left) + VSCode (40% middle) + WezTerm (20% right)
     swaymsg 'workspace number 1'
     swaymsg 'exec firefox'
     swaymsg 'exec code'
@@ -11,11 +11,11 @@ function startup
     while not swaymsg -t get_tree | grep -q '"name": "Visual Studio Code"'
         sleep 0.5
     end
-    swaymsg '[app_id="Firefox"] resize set width 33 ppt'
+    swaymsg '[app_id="Firefox"] resize set width 40 ppt'
     while not swaymsg -t get_tree | grep -q '"app_id": "org.wezfurlong.wezterm"'
         sleep 0.5
     end
-    swaymsg '[app_id="org.wezfurlong.wezterm"] resize set width 33 ppt'
+    swaymsg '[app_id="org.wezfurlong.wezterm"] resize set width 20 ppt'
 
     # Scratchpad notepad (floating, hidden until Super+`)
     swaymsg 'exec wezterm start --class notepad -- micro ~/Desktop/0-Inbox/notepad.md'
