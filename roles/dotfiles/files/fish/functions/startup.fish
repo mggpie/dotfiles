f0ff715d470aca5a3b4dac321151e25ae6986dd0
mggpie <mggpie@users.noreply.github.com>
2026-02-13 20:36:00 +0100

    wait for each window before launching next in startup

--- roles/dotfiles/files/fish/functions/startup.fish
+++ roles/dotfiles/files/fish/functions/startup.fish
@@ -6,11 +6,15 @@ function startup
     # Workspace 1: Firefox (40% left) + VSCode (40% middle) + WezTerm (20% right)
     swaymsg 'workspace number 1'
     swaymsg 'exec firefox'
-    sleep 1.5
+    while not swaymsg -t get_tree | grep -q '"app_id": "firefox"'
+        sleep 0.1
+    end
     swaymsg 'exec code'
-    sleep 1.5
-    swaymsg 'exec wezterm start -- fish -c notepad'
     while not swaymsg -t get_tree | grep -q '"name": "Visual Studio Code"'
+        sleep 0.1
+    end
+    swaymsg 'exec wezterm start -- fish -c notepad'
+    while not swaymsg -t get_tree | grep -q '"app_id": "org.wezfurlong.wezterm"'
         sleep 0.1
     end
     swaymsg '[app_id="Firefox"] resize set width 40 ppt'
