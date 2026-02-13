e73c2e97143cbab652c818d1823e80633778624a
mggpie <mggpie@users.noreply.github.com>
2026-02-13 17:48:53 +0100

    add translate function with super+t keybinding

---
+++ roles/dotfiles/files/fish/functions/translate.fish
@@ -0,0 +1,18 @@
+function translate --description "Translate selected text and copy to clipboard"
+    set -l selected (wl-paste -p 2>/dev/null)
+    
+    if test -z "$selected"
+        notify-send "Translate" "No text selected"
+        return 1
+    end
+
+    set -l translated (trans -b en:pl "$selected" 2>/dev/null)
+    
+    if test -z "$translated"
+        notify-send "Translate" "Translation failed"
+        return 1
+    end
+
+    echo -n "$translated" | wl-copy
+    notify-send "Translate" "$translated"
+end
